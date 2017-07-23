d223ae482c134206c228278a990c7d92c24d3d35
kennyzx <ikennyzx@gmail.com>
2017-07-23 21:04:17 +0800

    DVD Model/Controller

---
+++ MVCWebAPI/Controllers/DVDController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using MVCWebAPI.Models;
+
+namespace MVCWebAPI.Controllers
+{
+    public class DVDController : ApiController
+    {
+        public List<DVD> GetAllDVDsListedInStore()
+        {
+            return new List<DVD>()
+            {
+                new DVD { Title = "Win10 Home", Price = 99.99 },
+                new DVD { Title = "Win10 Pro", Price = 199.99 }
+            };
+        }
+    }
+}
